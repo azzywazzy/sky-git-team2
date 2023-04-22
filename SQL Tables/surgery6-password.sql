a8eb35b3d3f35544be7e44a419ba4f6fbf68c72c
Rachel Standish <rachelastandish@gmail.com>
2023-04-22 16:49:18 +0100

    just updating script 6 SQL

--- SQL Tables/surgery6-password.sql
+++ SQL Tables/surgery6-password.sql
@@ -4,8 +4,13 @@ CREATE TABLE credential(
 cred_id smallint not null primary key auto_increment,
 email varchar(50) not null,
 user_type tinyint not null, 
-hash_password varchar(50) not null
-)
+hash_password varchar(100) not null
+);
+
+insert into credential values (1, "MRauk@vets.com", "1", "lkjsalkj7;lkj;klj;aljj9");
+insert into credential values (2, "PJames@vets.com", "1", "lksdj;lgkajsd908olijdsf");
+insert into credential values (3, "MRai@vets.com", "1", "ldskjf;l89sdfskhj");
+insert into credential values (4, "fayequinn@email.com", "0", ";lksaf;lakopim;gd;l");
 
 
 
