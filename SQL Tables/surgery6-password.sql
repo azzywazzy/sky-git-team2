e5da176597377292ccd8a39d6f22ac20b1a15971
Julie <julie.kellachan@gmail.com>
2023-04-23 17:05:58 +0100

    edit to sql table - user_id

--- SQL Tables/surgery6-password.sql
+++ SQL Tables/surgery6-password.sql
@@ -1,7 +1,7 @@
 use surgery;
 
 CREATE TABLE credential(
-cred_id smallint not null primary key auto_increment,
+user_id smallint not null primary key auto_increment,
 email varchar(50) not null,
 user_type tinyint not null, 
 hash_password text not null
@@ -11,6 +11,7 @@ insert into credential values (1, "MRauk@vets.com", "1", "lkjsalkj7;lkj;klj;aljj
 insert into credential values (2, "PJames@vets.com", "1", "lksdj;lgkajsd908olijdsf");
 insert into credential values (3, "MRai@vets.com", "1", "ldskjf;l89sdfskhj");
 insert into credential values (4, "fayequinn@email.com", "0", ";lksaf;lakopim;gd;l");
+
 
 
 
