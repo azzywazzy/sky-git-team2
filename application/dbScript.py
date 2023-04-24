fd150bcc3477ec586a31a7d3d91e4d0da833cdc4
azzywazzy <arianne@hey.com>
2023-04-24 21:04:55 +0100

    updates to routes and templates

--- application/dbScript.py
+++ application/dbScript.py
@@ -7,8 +7,8 @@ from application.models.order_history import OrderHistory
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-user = 'root'
-password = ''
+user = 'workbench'
+password = 'password'
 host = '127.0.0.1'
 port = 3306
 database = 'Surgery'
--- application/routes.py
+++ application/routes.py
@@ -12,6 +12,65 @@ from application.models.credential import Credential
 from flask_login import login_user, current_user, logout_user
 
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Our Vet Surgery')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Credential.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.hash_password, form.hash_password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('all_customers'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'dangerous')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash('You are now logged out', 'success')
+    return redirect(url_for("login"))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        credential = Credential(email=form.cus_email.data, hash_password=hashed_password, user_type=0)
+        customer = Customer(cus_first_name=form.cus_first_name.data, cus_last_name=form.cus_last_name.data,
+                            cus_email=form.cus_email.data, address=form.address.data, phone=form.phone.data,
+                            cus_status=1)
+        db.session.add(credential)
+        db.session.add(customer)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/register-pet", methods=['GET', 'POST'])
+def register_patient():
+    form = PatientRegistrationForm()
+    if form.validate_on_submit():
+        patient = Patient(cus_id="1", pat_name=form.pat_name.data, species=form.species.data,
+                          breed=form.breed.data, sex=form.sex.data, date_of_birth=form.date_of_birth.data,
+                          weight=form.weight.data, chip_num=form.chip_num.data,
+                          neutered_status=form.neutered_status.data,
+                          has_insurance=form.has_insurance.data, )
+        db.session.add(patient)
+        db.session.commit()
+        flash('You have successfully added a pet!', 'success')
+        return redirect(url_for('all_products'))
+    return render_template('register_pet.html', title='Register Pet', form=form)
+
+
 @app.route("/admin/customers", methods=['GET'])
 def all_customers():
     error = ""
@@ -39,36 +98,6 @@ def all_patients():
     return render_template('admin-patients.html', patients=patients, message=error)
 
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        credential = Credential(email=form.cus_email.data, hash_password=hashed_password, user_type=0)
-        customer = Customer(cus_first_name=form.cus_first_name.data, cus_last_name=form.cus_last_name.data,
-                            cus_email=form.cus_email.data, address=form.address.data, phone=form.phone.data,
-                            cus_status=1)
-        db.session.add(credential)
-        db.session.add(customer)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = Credential.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.hash_password, form.hash_password.data):
-            login_user(user, remember=form.remember.data)
-            return redirect(url_for('all_customers'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'dangerous')
-    return render_template('login.html', title='Login', form=form)
-
-
 @app.route('/admin/products', methods=['GET'])
 def all_products():
     error = ""
@@ -78,30 +107,10 @@ def all_products():
     return render_template('admin-products.html', products=products, message=error)
 
 
-@app.route("/register-pet", methods=['GET', 'POST'])
-def register_patient():
-    form = PatientRegistrationForm()
-    if form.validate_on_submit():
-        patient = Patient(cus_id="1", pat_name=form.pat_name.data, species=form.species.data,
-                          breed=form.breed.data, sex=form.sex.data, date_of_birth=form.date_of_birth.data,
-                          weight=form.weight.data, chip_num=form.chip_num.data,
-                          neutered_status=form.neutered_status.data,
-                          has_insurance=form.has_insurance.data, )
-        db.session.add(patient)
-        db.session.commit()
-        flash('You have successfully added a pet!', 'success')
-        return redirect(url_for('all_products'))
-    return render_template('register_pet.html', title='Register Pet', form=form)
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    flash('You are now logged out', 'success')
-    return redirect(url_for("login"))
-
 # just trialling using the current_user value - you need to be logged in as someone for it to work
 @app.route("/trial", methods=['GET'])
 def trial():
     if current_user.is_authenticated:
         user = Customer.query.filter_by(cus_email=current_user.email).first()
         return [user.cus_email, user.cus_first_name, user.cus_last_name]
+    # return jsonify(products)
